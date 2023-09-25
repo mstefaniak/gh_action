17c94219c0b7b67f1ca1459a84762ac83d925e49
Marcin Stefaniak <marcin.stefaniak@plentific.com>
2023-09-25 09:26:59 +0200

    fix verificator

--- notify.sh
+++ notify.sh
@@ -6,17 +6,17 @@ IFS=',' read -ra NAMESPACES <<< "$2"
 ALLOWED_ENVS=( staging1 staging2 staging3 staging4 staging5 staging6 staging7 staging8 staging9 staging10 staging11 staging12 staging13 qa1 qa2 )
 ALLOWED_NAMESPACES=( uk de us )
 
-ERROR_MESSAGES = ()
+ERROR_MESSAGES=()
 
 if [[ ! " ${ALLOWED_ENVS[@]} " =~ " ${ENV} " ]]; then
   VALID=false
-  ERROR_MESSAGES += ("Not supported environment: '${ENV}', following environments are supported: ${ALLOWED_ENVS[@]}")
+  ERROR_MESSAGES+=("Not supported environment: '${ENV}', following environments are supported: ${ALLOWED_ENVS[@]}")
 fi
 
 for NAMESPACE in "${NAMESPACES[@]}"; do
   if [[ ! " ${ALLOWED_NAMESPACES[@]} " =~ " ${NAMESPACE} " ]]; then
     VALID=false
-    ERROR_MESSAGES += ("Not supported namespace(s) '${NAMESPACES}', following namespaces are supported: ${ALLOWED_NAMESPACES[@]}")
+    ERROR_MESSAGES+=("Not supported namespace(s) '${NAMESPACES}', following namespaces are supported: ${ALLOWED_NAMESPACES[@]}")
   fi
 done
 
