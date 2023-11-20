db290a7341d5ec8deb907aaa3b549e2dae109682
Marcin Stefaniak <marcin.stefaniak@plentific.com>
2023-11-20 12:44:17 +0100

    asdfsadf

--- message.js
+++ message.js
@@ -2,7 +2,7 @@ const { getMessage } = require('./getMessage')
 const { addOutput } = require('./addOutput')
 
 const result = process.argv[2]
-
+console.log('debug: result', result)
 const { body, reactions } = getMessage(result)
 addOutput('body', body)
 addOutput('reactions', reactions)
